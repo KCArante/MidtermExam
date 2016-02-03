c8adfebd3bfdfb34404af7bbbd1a9d66c6641952
KCArante <kc.arante@gmail.com>
2016-02-02 17:19:00 -0800

    Getting the input from the user using the actionSearch imeOption

--- app/src/main/java/sidespell/tech/midtermexam/fragments/MainFragment.java
+++ app/src/main/java/sidespell/tech/midtermexam/fragments/MainFragment.java
@@ -3,13 +3,22 @@ package sidespell.tech.midtermexam.fragments;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import sidespell.tech.midtermexam.R;
 
 public class MainFragment extends Fragment {
+
+
+    private EditText etInput;
+    String input;
 
     public static MainFragment newInstance() {
         return new MainFragment();
@@ -21,12 +30,32 @@ public class MainFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_main, container, false);
 
         // TODO: Find all views here..
+        etInput = (EditText)view.findViewById(R.id.etAlbum);
+        etInput.setImeOptions(EditorInfo.IME_ACTION_SEARCH);
+
+        input = etInput.getText().toString();
+
+        etInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
 
         return view;
     }
+
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         // TODO: Perform logic operations here..
     }
+
+    public void performSearch(){
+    //    Toast.makeText(getActivity(), etInput.getText() + " Inputted!", Toast.LENGTH_SHORT).show();
+    };
 }
